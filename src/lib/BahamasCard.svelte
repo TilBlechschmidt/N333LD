6be036b106a6b57ffd2adcbed2858bfb1b7cc244
Til Blechschmidt <til@blechschmidt.de>
2022-12-05 14:22:19 +0000

    Fix content overflowing page

--- src/lib/BahamasCard.svelte
+++ src/lib/BahamasCard.svelte
@@ -6,19 +6,17 @@
 	export let delay = 0;
 </script>
 
-<div
-	class="card p-8 absolute top-0 right-0 max-w-lg m-16"
-	in:fly={{ y: 400, delay }}
-	out:fly={{ y: -400 }}
->
-	<div class="text-2xl"><slot name="title" /></div>
-	<hr class="my-4" />
-	<div class="mb-4"><slot /></div>
-	<div class="flex">
-		<div class="grow" />
-		<IconButton on:click>
-			Continue
-			<MdArrowForward slot="icon" />
-		</IconButton>
+<div class="absolute top-0 right-0 h-full overflow-y-auto">
+	<div class="card p-8 max-w-lg m-16" in:fly={{ y: 400, delay }} out:fly={{ y: -400 }}>
+		<div class="text-2xl"><slot name="title" /></div>
+		<hr class="my-4" />
+		<div class="mb-4"><slot /></div>
+		<div class="flex">
+			<div class="grow" />
+			<IconButton on:click>
+				Continue
+				<MdArrowForward slot="icon" />
+			</IconButton>
+		</div>
 	</div>
 </div>
